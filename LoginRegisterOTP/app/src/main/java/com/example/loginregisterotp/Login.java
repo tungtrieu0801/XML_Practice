3b3af5bcaecb6a68b523e63ca76787a5bb9d8f87
tungtrieu0801 <trieutungvp@gmail.com>
2024-03-03 10:59:24 +0700

    init

--- LoginRegisterOTP/app/src/main/java/com/example/loginregisterotp/Login.java
+++ LoginRegisterOTP/app/src/main/java/com/example/loginregisterotp/Login.java
@@ -2,13 +2,48 @@ package com.example.loginregisterotp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    boolean passwordshowing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText usernameedt = findViewById(R.id.usernameEDT);
+        final EditText passwordedt = findViewById(R.id.passEDT);
+        final ImageView passwordIcon = findViewById(R.id.passwordIcon);
+        final TextView signUp = findViewById(R.id.SignUpBtn);
+
+        passwordIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(passwordshowing){
+                    passwordshowing = false;
+                    passwordedt.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passwordIcon.setImageResource(R.drawable.eye_outline);
+                }else {
+                    passwordshowing = true;
+                    passwordedt.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passwordIcon.setImageResource(R.drawable.eye_off_outline);
+                }
+                passwordedt.setSelection(passwordedt.length());
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
     }
 }
